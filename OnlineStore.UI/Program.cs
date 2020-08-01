9eddd392109dfbaa436c009a355b1ddfe395d930
Elmer Datolayta <elmer.datolayta@devpartners.co>
2020-08-01 22:03:56 +0800

    Add project with encapsulation

---
+++ OnlineStore.UI/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.UI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+            string name = "Elmer";
+            string username = "datzme101";
+            string password = "TempPassword";
+            bool isUserSaved = user.Register(name, username, password);
+            if (isUserSaved)
+            {
+                Console.WriteLine("Successfully Saved!!!.");
+            }
+        }
+    }
+}
---
+++ OnlineStore.UI/User.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineStore.UI
+{
+    class User
+    {
+        private bool SaveRegistration(string name, string username, string encryptedPassword)
+        {
+            // Save user info
+            Console.WriteLine("Name: {0}", name);
+            Console.WriteLine("Username: {0}", username);
+            Console.WriteLine("Password: {0}", encryptedPassword);
+            return true;
+        }
+
+        private string EncryptPassword(string password)
+        {
+            // Do password encryption
+            string encryptedPassword = "56857cfc709d3996f057252c16ec4656f5292802";
+            return encryptedPassword;
+        }
+
+        public bool Register(string name, string username, string password)
+        {
+            string encryptedPassword = this.EncryptPassword(password);
+            return this.SaveRegistration(name, username, encryptedPassword);
+        }
+    }
+}
